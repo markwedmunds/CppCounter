2a3ac223fc41fbe10c801ff4789e47bb93c24b89
Mark Edmunds <mark.edmunds@hybrid-designs.co.uk>
2023-09-21 11:01:35 +0100

    Initial Commit

---
+++ CppCounter/CppCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CppCounterApp.swift
+//  CppCounter
+//
+//  Created by Mark Edmunds on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CppCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
